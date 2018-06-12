a9851f4ab406d87e1c95bd8b3221e939e298c834
Beach <dbeach@languageline.com>
2018-06-12 09:43:21 -0700

    stuf3?

--- website.py
+++ website.py
@@ -9,3 +9,8 @@ def hello():
 @app.route('/random')
 def random():
     return render_template('random.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0',port=5000)
